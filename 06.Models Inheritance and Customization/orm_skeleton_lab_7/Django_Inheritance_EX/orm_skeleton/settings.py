37dfd45df993154053c01c5a9f7f16bfa9456b1e
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-11-08 11:18:23 +0000

    Added new solutions

--- 06.Models Inheritance and Customization/orm_skeleton_lab_7/Django_Inheritance_EX/orm_skeleton/settings.py
+++ 06.Models Inheritance and Customization/orm_skeleton_lab_7/Django_Inheritance_EX/orm_skeleton/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "08-Python-ORM-Exercise",
         "USER": "postgres",
-        "PASSWORD": "mark881226",
+        "PASSWORD": "postgres",
         "HOST": "localhost",
         "PORT": "5432",
     }
